04e186ed76aa1e3421ba2f3f7c61f7a76244b71e
Mateusz <matesz1kb@gmail.com>
2021-04-20 15:44:41 +0200

    Removed not used code

--- js/auth.js
+++ js/auth.js
@@ -1,10 +1,7 @@
 // Listen for auth status changes
 auth.onAuthStateChanged(user => {
   if (user) {
-    user.getIdTokenResult().then(idTokenResult => {
-      user.admin = idTokenResult.claims.admin;
       setupUI(user);
-    });
     db.collection('guides').onSnapshot(snapshot => {
       setupGuides(snapshot.docs);
     }, err => console.log(err.message));
