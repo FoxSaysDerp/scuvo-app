e24491a2482a579e3816641ffa172a8489b4e510
Mateusz <matesz1kb@gmail.com>
2021-04-27 13:58:37 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
     // Initialize Firebase
 var firebaseConfig = {
-  apiKey: "AIzaSyC0SF64zoErXGMWEJvQ__hZmrkbKPAklpc",
-  authDomain: "scuvo-cfc00.firebaseapp.com",
-  projectId: "scuvo-cfc00",
-  storageBucket: "scuvo-cfc00.appspot.com",
-  messagingSenderId: "815366829942",
-  ppId: "1:815366829942:web:52cd42c67106352507ec57"
+  apiKey: "X,
+  authDomain: "X",
+  projectId: "X",
+  storageBucket: "X",
+  messagingSenderId: "0",
+  ppId: "0"
 };
 firebase.initializeApp(firebaseConfig);
     
